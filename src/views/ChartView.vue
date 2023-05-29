fc3a76a057c6e96f855553214324fef08ca59e78
Alexnander Nasokin <alexnas2python@gmail.com>
2023-05-29 13:39:11 +0300

    Fully rewrite the whole astroData store logic (and views accordingly)

--- src/views/ChartView.vue
+++ src/views/ChartView.vue
@@ -1,39 +1,27 @@
 <script setup lang="ts">
+import { onMounted } from 'vue'
 import { storeToRefs } from 'pinia'
-import DataCard from '@/components/DataCard.vue'
 import ChartCard from '@/components/ChartCard.vue'
 import { useAstroDataStore } from '@/stores/astroData'
 
 const astroDataStore = useAstroDataStore()
-const { initObj, convertedRes, idsByLevel, allLevelsFlatTotal, baseLevel } =
-  storeToRefs(astroDataStore)
+const { convertedRes, baseLevel } = storeToRefs(astroDataStore)
+
+onMounted(() => {
+  astroDataStore.getDummyAstroData()
+  astroDataStore.getAstroFromZodiac()
+  astroDataStore.getAstroData()
+  astroDataStore.getResIdsObj()
+})
 </script>
 
 <template>
   <main class="mx-auto max-w-screen-xl">
-    <h1 class="text-3xl font-bold underline text-teal-400 text-center my-4">Astro Show Chart</h1>
+    <h1 class="text-3xl font-bold underline text-teal-400 text-center my-4">Astro Chart: AAA</h1>
     <div class="px-4">
-      <ChartCard title="Result Astro Data" :data="convertedRes" :show="baseLevel" />
-    </div>
-
-    <h1 class="text-3xl font-bold underline text-teal-400 text-center my-4">Computed Data</h1>
-    <div class="grid grid-cols-2 gap-4 px-4">
-      <div>
-        <h2 class="text-2xl font-medium text-teal-400 text-center">Result Data</h2>
-        <div class="flex-col space-y-4">
-          <DataCard title="Result Astro Data" :data="convertedRes" />
-
-          <hr class="w-full h-1 py-3 bg-teal-200 border-0 rounded dark:bg-gray-700" />
-        </div>
-      </div>
-      <div>
-        <h2 class="text-2xl font-medium text-teal-400 text-center">Initial Data</h2>
-        <div class="flex-col space-y-4">
-          <DataCard title="All Parent IDs by Level" :data="idsByLevel" />
-          <DataCard title="Parent IDs Flat TOTAL" :data="allLevelsFlatTotal" />
-          <DataCard title="Initial Astro Data" :data="initObj" />
-        </div>
-      </div>
+      <ChartCard title="Chart Stage 1" :data="convertedRes" :show="baseLevel" />
+      <ChartCard title="Chart Stage 2" :data="convertedRes" :show="baseLevel" />
+      <ChartCard title="Chart Stage 3" :data="convertedRes" :show="baseLevel" />
     </div>
   </main>
 </template>
