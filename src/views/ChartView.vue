2898813b2f4e0999a51d4a5f63327677165081e5
Alexnander Nasokin <alexnas2python@gmail.com>
2023-06-01 00:02:14 +0300

    Styling - align and beautify appearance of two charts design

--- src/views/ChartView.vue
+++ src/views/ChartView.vue
@@ -13,44 +13,52 @@ onMounted(() => {
 </script>
 
 <template>
-  <main class="grid grid-cols-2 mx-auto max-w-screen-xl">
-    <div>
-      <h1 class="text-3xl font-bold underline text-teal-400 text-center my-4">
-        Astro Chart: Pers1
-      </h1>
-      <div class="px-4">
+  <main class="px-3 mx-auto max-w-screen-xl">
+    <div class="my-6 grid grid-cols-2 text-3xl font-bold underline text-teal-400 text-center">
+      <h1 class="">Astro Chart: Pers1</h1>
+      <h1 class="">Astro Chart: Pers2</h1>
+    </div>
+    <div class="grid grid-cols-2 grid-flow-row gap-6 border-b-4">
+      <div>
         <ChartCard
           title="Chart 1: Stage 1"
           :data="currentDoubleAstroData[0].convertedResAllGrades[0]"
           :baseLevel="currentDoubleAstroData[0].baseLevelAllGrades[0]"
         />
+      </div>
+      <div>
+        <ChartCard
+          title="Chart 2: Stage 1"
+          :data="currentDoubleAstroData[1].convertedResAllGrades[0]"
+          :baseLevel="currentDoubleAstroData[1].baseLevelAllGrades[0]"
+        />
+      </div>
+    </div>
+    <div class="grid grid-cols-2 grid-flow-row gap-6 border-b-4">
+      <div>
         <ChartCard
           title="Chart 1: Stage 2"
           :data="currentDoubleAstroData[0].convertedResAllGrades[1]"
           :baseLevel="currentDoubleAstroData[0].baseLevelAllGrades[1]"
         />
+      </div>
+      <div>
+        <ChartCard
+          title="Chart 2: Stage 2"
+          :data="currentDoubleAstroData[1].convertedResAllGrades[1]"
+          :baseLevel="currentDoubleAstroData[1].baseLevelAllGrades[1]"
+        />
+      </div>
+    </div>
+    <div class="grid grid-cols-2 grid-flow-row gap-6">
+      <div>
         <ChartCard
           title="Chart 1: Stage 3"
           :data="currentDoubleAstroData[0].convertedResAllGrades[2]"
           :baseLevel="currentDoubleAstroData[0].baseLevelAllGrades[2]"
         />
       </div>
-    </div>
-    <div>
-      <h1 class="text-3xl font-bold underline text-teal-400 text-center my-4">
-        Astro Chart: Pers2
-      </h1>
-      <div class="px-4">
-        <ChartCard
-          title="Chart 2: Stage 1"
-          :data="currentDoubleAstroData[1].convertedResAllGrades[0]"
-          :baseLevel="currentDoubleAstroData[1].baseLevelAllGrades[0]"
-        />
-        <ChartCard
-          title="Chart 2: Stage 2"
-          :data="currentDoubleAstroData[1].convertedResAllGrades[1]"
-          :baseLevel="currentDoubleAstroData[1].baseLevelAllGrades[1]"
-        />
+      <div>
         <ChartCard
           title="Chart 2: Stage 3"
           :data="currentDoubleAstroData[1].convertedResAllGrades[2]"
